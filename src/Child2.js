bd610443e6931ed38cf9127b3623ae34e94bc09c
sajid-khan-afridi <69663016+sajid-khan-afridi@users.noreply.github.com>
2020-12-02 17:17:16 +0500

    Update Child2.js

--- src/Child2.js
+++ src/Child2.js
@@ -6,7 +6,7 @@ const Child2 = () => {
     console.log(state)
     return(
         <div>
-                <h2>This is second chil  d using Counter Reducer</h2>
+                <h2>This is second child using Counter Reducer</h2>
                 <h3>Value of reducer state is: {state} </h3>
                 <button onClick={()=>dispatch('INCREMENT')}>Increment Reducer</button>
                 <br />      
